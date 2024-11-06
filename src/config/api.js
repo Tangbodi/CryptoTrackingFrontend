b389ec5296984eecce1dfe367c3592c4020163bc
tangbodi <tangbodi@gmail.com>
2024-11-06 14:57:39 -0500

    added PlaceOrder component

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,1 @@
 
-export const CoinMarketCap_API_KEY = "7b15aedc-a38a-4acb-9168-835013abfba6"
-export const CoinMarketCap_Listings_Latest_API = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
